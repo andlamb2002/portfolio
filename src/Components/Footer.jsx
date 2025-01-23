ca03f2fc76411def8fd18a486a7ee407d22b4711
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-01-23 01:48:19 -0500

    Spacing

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
 
                 <span>Built with React by Andreas Lambropoulos, {year}</span>
 
-                <ul className="flex space-x-4 mt-2">
+                <ul className="flex space-x-6 mt-4">
                     <li><a href="https://github.com/andlamb2002" target="_blank" rel="noopener noreferrer">GitHub</a></li>
                     <li><a href="https://www.linkedin.com/in/andreas-k-lambropoulos/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
                     <li><a href="https://www.youtube.com/@andreaslambropoulos1159" target="_blank" rel="noopener noreferrer">YouTube</a></li>
