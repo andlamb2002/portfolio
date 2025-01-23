1bbb51a8ecfe7f3877cf489478c70d799690035e
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-01-23 03:14:58 -0500

    Projects page

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
 function Projects() {
-  return (
-    <div>Projects</div>
-  )
+    return (
+        <div className="flex justify-center min-h-screen bg-gray-300">
+          <div className="text-center">
+            <h1 className="text-3xl font-bold mb-4">Projects</h1>
+            <p className="text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+    )
 }
 
 export default Projects
