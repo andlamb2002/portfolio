ad916545addf7d230253bf0c68860f7635712542
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-01-23 01:45:43 -0500

    Header and Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    const year = new Date().getFullYear();  
+
+    return (
+        <div className="bg-gray-200 py-6">
+            <div className="px-6 flex flex-col items-center">
+
+                <span>Built with React by Andreas Lambropoulos, {year}</span>
+
+                <ul className="flex space-x-4 mt-2">
+                    <li><a href="https://github.com/andlamb2002" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                    <li><a href="https://www.linkedin.com/in/andreas-k-lambropoulos/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                    <li><a href="https://www.youtube.com/@andreaslambropoulos1159" target="_blank" rel="noopener noreferrer">YouTube</a></li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Footer
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,19 +3,24 @@ import { Link } from 'react-router-dom';
 
 
 function Header() {
-  return (
-    <div className="sticky">
-        Header
-        <nav>
-            <ul className="flex space-x-4">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            </ul>
-        </nav>
-    </div>
-  )
+    return (
+        <div className="sticky top-0 w-full bg-gray-200 py-6">
+            <div className="px-6 flex justify-between items-center">
+                <div className="text-left">
+                    <Link to="/" className="font-bold">
+                        Logo
+                    </Link>
+                </div>
+                <nav>
+                    <ul className="flex space-x-6">
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/projects">Projects</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    )
 }
 
 export default Header
