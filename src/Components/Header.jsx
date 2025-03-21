eb9074f223478a9b94aef1db09faf5674a1a06af
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-03-21 12:27:05 -0400

    Fixed portfolio link

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
                 <nav>
                     <ul className="flex space-x-6">
                         <li><Link to="/about">About</Link></li>
-                        <li><Link to="/projects">Projects</Link></li>
+                        <li><Link to="/portfolio">Projects</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
                     </ul>
                 </nav>
