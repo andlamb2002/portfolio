19c1a917d88208425d90e3365909869e2094cd48
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-03-22 00:44:19 -0400

    Added hover colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,9 @@ export default {
 				primary: '#1F2841',
 				secondary: '#374D62',
 				button: '#04594B',
+				buttonHover: '#044439',
 				text: '#ECEDEE',
+				textHover: '#D5D6D7',
 			},
 			fontFamily: {
 				sans: ['"Open Sans"', 'sans-serif'],
