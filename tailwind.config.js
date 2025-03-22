6ab7f0e08187a00c5c5d5fa0b77f36cb23297ce8
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-03-21 23:58:28 -0400

    Color and typography setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  	content: [
+    	"./index.html",
+    	"./src/**/*.{js,jsx,ts,tsx}",
+  	],
+  	theme: {
+    	extend: {
+			colors: {
+				primary: '#1F2841',
+				secondary: '#374D62',
+				button: '#04594B',
+				text: '#ECEDEE',
+			},
+			fontFamily: {
+				sans: ['"Open Sans"', 'sans-serif'],
+			},
+    	},
+  	},
+  	plugins: [],
 };
