1d60476d0cbf3dfaef965dcbebc4341a48b564ae
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-03-23 19:24:05 -0400

    Slight header spacing change

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
             <div className="flex flex-col md:flex-row justify-between items-center w-4/5 mx-auto p-8 text-center md:text-left">
                 <Link to="/" className="text-4xl font-bold hover:underline hover:text-textHover md:pr-16"> Andreas Lambropoulos </Link>
                 <nav>
-                    <ul className="flex flex-col md:flex-row gap-x-8 text-xl mt-4 md:mt-0">
+                    <ul className="flex flex-col md:flex-row gap-y-2 md:gap-y-0 gap-x-8 text-xl mt-4 md:mt-0">
                         <li><Link to="/about" className="hover:underline hover:text-textHover"> About </Link></li>
                         <li><Link to="/portfolio" className="hover:underline hover:text-textHover"> Portfolio </Link></li>
                         <li><Link to="/contact" className="hover:underline hover:text-textHover"> Contact </Link></li>
