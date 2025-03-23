a0eded1705159e031dcc5d07834567f0f5950d1b
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-03-23 19:07:23 -0400

    Responsive header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <div className="sticky top-0 w-full bg-secondary">
-            <div className="flex justify-between items-center w-4/5 mx-auto p-8">
-                <Link to="/" className="text-4xl font-bold hover:underline hover:text-textHover pr-16"> Andreas Lambropoulos </Link>
+            <div className="flex flex-col md:flex-row justify-between items-center w-4/5 mx-auto p-8 text-center md:text-left">
+                <Link to="/" className="text-4xl font-bold hover:underline hover:text-textHover md:pr-16"> Andreas Lambropoulos </Link>
                 <nav>
-                    <ul className="flex gap-x-8 text-xl">
+                    <ul className="flex flex-col md:flex-row gap-x-8 text-xl mt-4 md:mt-0">
                         <li><Link to="/about" className="hover:underline hover:text-textHover"> About </Link></li>
                         <li><Link to="/portfolio" className="hover:underline hover:text-textHover"> Portfolio </Link></li>
                         <li><Link to="/contact" className="hover:underline hover:text-textHover"> Contact </Link></li>
