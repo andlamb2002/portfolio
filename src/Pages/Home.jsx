671f457be0d18026b87aea5e5b003f46e33f6c2c
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-03-23 19:19:39 -0400

    slight fix to responsive homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,9 +12,9 @@ function Home() {
                     <img src={homeIntro.img} alt="Headshot" className="w-full h-full object-cover" />
                 </div>
 
-                <div className="text-left md:ml-8">
-                    <h1 className="text-4xl font-bold mt-4 md:mt-0">{homeIntro.greeting}</h1>
-                    <p className="text-lg mt-4">
+                <div className="md:ml-8">
+                    <h1 className="text-4xl font-bold mt-4 md:mt-0 text-center md:text-left">{homeIntro.greeting}</h1>
+                    <p className="text-lg text-left mt-4">
                         {homeIntro.intro}
                     </p>
                 </div>
