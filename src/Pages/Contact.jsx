60cd6d8419b90c7a37da9e914a773ef22675196b
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-05-13 12:16:03 -0400

    Small contact page update

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,7 +3,7 @@ function Contact() {
         <section className="mx-auto py-8">
         <h2 className="text-4xl font-bold underline text-left">Contact</h2>
         <div className="mt-8 text-lg">
-            Contact page is still being developed. You can send me an email at{" "}
+            You can send me an email at{" "}
             <a
                 href="mailto:andlamb2002@gmail.com"
                 className="hover:text-textHover underline"
