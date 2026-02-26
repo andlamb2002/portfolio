92106f3d0c6b442bd2acf3001f270234ac6ce74f
Andreas Lambropoulos <andlamb2002@gmail.com>
2026-02-26 02:56:49 -0500

    Removed underlines

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
     
     return (
         <section className="mx-auto py-8">
-            <h2 className="text-4xl font-bold underline text-left">About Me</h2>
+            <h2 className="text-4xl font-bold text-left">About Me</h2>
             <div className="flex flex-col gap-y-8 mt-8">
                 {aboutSections.map((section, index) => (
                     <AboutSection key={index} img={section.img} alt={section.alt} text={section.text}/>
